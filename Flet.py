7cd5eef5da5971d58dc9e748fe42e084a231affc
Vasiliy Zolotenkov <zolotenkovvasilij2007@gmail.com>
2025-12-03 19:05:42 +0300

    Registration_Window

--- Flet.py
+++ Flet.py
@@ -2,7 +2,20 @@ import flet as  ft
 
 def main(page: ft.Page):
     page.title = "Flet App"
-    page.theme_mode
+    page.theme_mode = 'Light' #Or Dark
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    page.add(
+        ft.Row(
+            [
+                ft.Text('Заргистрируйтесь'),
+                ft.TextField(value = "0",width = 150, text_align=ft.TextAlign.CENTER),
+                ft.IconButton(ft.Icons.CANCEL),
+                ft.Icon(ft.Icons.BACK_HAND)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
 
 
 ft.app(target=main)
